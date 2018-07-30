aba096f0084677b778aceeeef1ab9e9e19d36634
lipc <3236568459@qq.com>
2018-07-30 21:08:28 +0800

    网上的例子

--- mqtt_server.js
+++ mqtt_server.js
@@ -1,16 +1,16 @@
 var mosca = require('mosca');
 
-// var ascoltatore = {
-//   //using ascoltatore
-//   type: 'mongo',
-//   url: 'mongodb://localhost:27017/mqtt',
-//   pubsubCollection: 'ascoltatori',
-//   mongo: {}
-// };
+var ascoltatore = {
+  //using ascoltatore
+  type: 'mongo',
+  url: 'mongodb://localhost:27017/mqtt',
+  pubsubCollection: 'ascoltatori',
+  mongo: {}
+};
 
 var settings = {
   port: 1883,
-  //backend: ascoltatore
+  backend: ascoltatore
 };
 
 var server = new mosca.Server(settings);
