7bf55d3133352463866de58875164f363535aeb7
lipc <3236568459@qq.com>
2018-08-18 14:16:47 +0800

    让mqtt服务器把daily1的数据保存到一个专用的collection中:daily_1"

--- mqtt_server.js
+++ mqtt_server.js
@@ -2,17 +2,17 @@ var mosca = require('mosca');
 var StringDecoder = require('string_decoder').StringDecoder;
 var decoder = new StringDecoder('utf-8');
 
-var ascoltatore = {
-  //using ascoltatore
-  type: 'mongo',
-  url: 'mongodb://localhost:27017/mqtt',
-  pubsubCollection: 'ascoltatori',
-  mongo: {}
-};
+// 原来的数据库的名字是: ascoltatori',
+var mongodbSetting = {
+  type:'mongo',
+  url:'mongodb://10.66.4.189:2017/mqtt',
+  pubsubCollection:"mqtt_sub_pub_log",
+  mongo:{}
+}
 
 var settings = {
   port: 1883,
-  backend: ascoltatore
+  backend: mongodbSetting
 };
 
 var server = new mosca.Server(settings);
@@ -21,10 +21,38 @@ server.on('clientConnected', function(client) {
     console.log('client connected,client id:', client.id);
 });
 
+
+// 要保存的数据的topic:
+saving_one = "daily_1";
 // fired when a message is received
 server.on('published', function(packet, client) {
-  console.log(client, ' Published topic:', packet.topic,' payload:',decoder.write(Buffer.from(packet.payload))); // packet.payload.toString()
+  console.log(' Published topic:', packet.topic,' payload:',decoder.write(Buffer.from(packet.payload))); // packet.payload.toString()
+  if(saving_one == packet.topic){
+    saveMessageToDB(packet.payload.toString);
+  }
 });
+
+//  ===============================================================================================
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://10.66.4.189:27017/mqtt";
+var collection_handle = null;
+MongoClient.connect(url, function(err, db) {
+  if(err){
+    console.log('connect to mqtt db fail!!');
+    throw err;
+  }else{
+    console.log('db connected!!');
+    collection_handle = db.collection(saving_one); 
+  }
+});
+function saveMessageToDB(save_str){
+  json = JSON.parse(save_str);
+  console.log(json);
+  json.timestamp = parseInt(Date.parse(new Date())/1000);
+  collection_handle.insert(json,function(err,result){
+    console.log('err:',err," result:",result);
+  });
+}
 
 server.on('ready', setup);
 
