9c80a71500609e50da5ecb99b581bdf87f48f252
lipc <3236568459@qq.com>
2018-09-02 16:05:59 +0800

    改了web.js 添加了静态文件服务

--- web.js
+++ web.js
@@ -99,6 +99,8 @@ client.on('message', function (topic, message) {
 // 	})
 // });
 
+app.use("*",express.static( path.join(__dirname,'box_controller_web')));
+
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
