4d157410f3cd87364ea66ee440fbacf2275656ee
lipc <3236568459@qq.com>
2018-09-02 17:21:33 +0800

    fixing bug..

--- web.js
+++ web.js
@@ -99,18 +99,18 @@ client.on('message', function (topic, message) {
 // 	})
 // });
 var path = require('path');
-app.use("*", function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  if (req.method === 'OPTIONS') {
-    res.send(200)
-  } else {
-    next()
-  }
+//设置跨域访问
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
 });
 
-app.use(express.static( path.join(__dirname,'box_controller_web')));
+
+//app.use(express.static( path.join(__dirname,'box_controller_web')));
 
 
 
